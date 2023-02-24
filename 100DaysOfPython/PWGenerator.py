087b15a03f9ce820b426a5baa702637efb600313
Goblinmine <jojo.millonig@gmail.com>
2023-02-24 10:39:55 +0100

    Change PWgen string scrambler

--- 100DaysOfPython/PWGenerator.py
+++ 100DaysOfPython/PWGenerator.py
@@ -29,11 +29,13 @@ for x in range(numbers):
     output += RandNumber()
 
 if input("Would you like to use the advanced version? Y/N ").upper() == 'Y':
+    
+    howMany = len(output) -1
     outputArray = list(output)
     output = ""
-    for i in range(len(outputArray)-1):
-        iToRemove = secrets.randbelow(len(outputArray))
-        output += outputArray[i]
-        outputArray.remove(outputArray[i])
+    for i in range(howMany):
+        iSelected = secrets.randbelow(len(outputArray) -1)
+        output += outputArray[iSelected]
+        outputArray.remove(outputArray[iSelected])
         
 print(f"The Password is {output}")
