72f3aa513d1f40a1b60f5797c675fe7ffcf3957b
Goblinmine <jojo.millonig@gmail.com>
2023-03-01 12:08:38 +0100

    Finished 8.2

--- 100DaysOfPython/8.2_PrimeNumbers.py
+++ 100DaysOfPython/8.2_PrimeNumbers.py
@@ -5,7 +5,13 @@
 #Write your code below this line 👇
 
 
-def prime_checker(number):
+def prime_checker(number: int):
+    if number == 2 or number == 3 or number == 5 or number == 7:
+        print("It's a prime number.")
+    elif number % 2 == 0 or number % 3 == 0 or number % 5 == 0 or number % 7 == 0:
+        print("It's not a prime number.")
+    else:
+        print("It's a prime number.")
     
 
 
