124e2135102987e63c778572c76cf148a08d144d
Goblinmine <jojo.millonig@gmail.com>
2023-03-01 13:02:46 +0100

    added python decorator

--- 100DaysOfPython/54.1_CreateYourOwnPythonDecorator.py
+++ 100DaysOfPython/54.1_CreateYourOwnPythonDecorator.py
@@ -1,14 +1,18 @@
 # Exercise:         Day 54.1 | Create Your Own Python Decorator
 # Created by:       Jonas Millonig
-# Creation date:    
+# Creation date:    01.03.2023
 
 import time
 current_time = time.time()
 print(current_time)
 
-def speed_calc_decorator():
-    pass
-
+def speed_calc_decorator(function, dec = 2):
+    start_time = time.time()
+    function()
+    stop_time = time.time()
+    speed = round(stop_time - start_time, dec)
+    return f"{function.__name__} run speed: {speed}s"
+    
 def fast_function():
     for i in range(10000000):
         i * i
@@ -16,3 +20,9 @@ def fast_function():
 def slow_function():
     for i in range(100000000):
         i * i
+        
+      
+      
+print(speed_calc_decorator(fast_function, 5))
+print(speed_calc_decorator(slow_function))
+    
