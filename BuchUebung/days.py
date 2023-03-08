84a132e4f534dbd3cf02220f4efdc9b914bd7cf8
Goblinmine <jojo.millonig@gmail.com>
2023-03-08 11:50:53 +0100

    cleanup days.py

--- BuchUebung/days.py
+++ BuchUebung/days.py
@@ -7,16 +7,11 @@ letzterTagVorjahr = int(input("Was war der letzte tag im vorjahr? 1-7 \n"))
 isLeapYear = int(input("War das letzte jahr ein Schaltjar? 0 / 1\n"))
 
 
-if isLeapYear:
-    daysInYear = 366
-    daysPerMonth = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-else :
-    daysInYear = 365
-    daysPerMonth = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+if isLeapYear: daysPerMonth = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+else : daysPerMonth = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 
 output = 0
 dayCounter = letzterTagVorjahr
-
 
 for days in reversed(daysPerMonth):                                             # goes 12 through that loop; start with dec.
     for day in reversed(range(1, days +1)):                                     # goes 31/30/29/28 days through; start with last day
