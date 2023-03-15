5fcb66054706be00047e47fe274051790df1de2c
Goblinmine <jojo.millonig@gmail.com>
2023-03-15 09:55:54 +0100

    fixed some more Spaces:

--- 100DaysOfPython/9.1_GradingProgram.py
+++ 100DaysOfPython/9.1_GradingProgram.py
@@ -3,11 +3,11 @@
 # Creation date: 
    
 student_scores = {
-  "Harry": 81,
-  "Ron": 78,
-  "Hermione": 99, 
-  "Draco": 74,
-  "Neville": 62,
+    "Harry": 81,
+    "Ron": 78,
+    "Hermione": 99, 
+    "Draco": 74,
+    "Neville": 62,
 }
 # 🚨 Don't change the code above 👆
 
@@ -15,7 +15,7 @@ student_scores = {
 student_grades = {}
 
 
-#TODO-2: Write your code below to add the grades to student_grades.👇
+# TODO-2: Write your code below to add the grades to student_grades.👇
 
 for student in student_scores:
     grade = ""
