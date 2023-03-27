140e6360380893d74dd75028dbb6c54b55877248
Goblinmine <jojo.millonig@gmail.com>
2023-03-27 15:53:58 +0200

    added some testing code

--- stack_queue.py
+++ stack_queue.py
@@ -36,4 +36,29 @@ class Queue:
     
     def is_empty(self):
         return True if len(self.__liste) == 0 else False
-    
+
+
+
+my_queue = Queue()
+my_queue.enqueue("first item")
+print(f"Queue: {my_queue}")
+print(f"Queue emty?: {my_queue.is_empty()}")
+my_queue.enqueue("second item")
+print(f"Queue: {my_queue}")
+print(f"Queue dequeue: {my_queue.dequeue()}")
+print(f"Queue: {my_queue}")
+print(f"Queue dequeue: {my_queue.dequeue()}")
+print(f"Queue: {my_queue}")
+print(f"Queue emty?: {my_queue.is_empty()}")
+
+my_stack = Stack()
+my_stack.push("first item")
+print(f"Stack: {my_stack}")
+print(f"Stack emty?: {my_stack.is_empty()}")
+my_stack.push("second item")
+print(f"Stack: {my_stack}")
+print(f"Stack pop: {my_stack.pop()}")
+print(f"Stack: {my_stack}")
+print(f"Stack pop: {my_stack.pop()}")
+print(f"Stack: {my_stack}")
+print(f"Stack emty?: {my_stack.is_empty()}")
