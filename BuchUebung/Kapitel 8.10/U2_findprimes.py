f6229647b0deb03613b1dac8cb24932558451b59
Goblinmine <jojo.millonig@gmail.com>
2023-03-28 08:44:40 +0200

    changed U2 a little. added flowchart

--- BuchUebung/Kapitel 8.10/U2_findprimes.py
+++ BuchUebung/Kapitel 8.10/U2_findprimes.py
@@ -6,7 +6,9 @@
 user_input = int(input("Pick a number to count to: "))
 numbers = [number for number in range(1, user_input + 1)]
 
-for i in numbers: numbers = [0 if i > 1 and number > i and number % i == 0 else number for number in numbers]
-while numbers.__contains__(0): numbers.remove(0)
+for i in numbers: numbers = [number for number in numbers if not (i > 1 and number > i and number % i == 0)]
+
+# for i in numbers: numbers = [0 if i > 1 and number > i and number % i == 0 else number for number in numbers]
+# while numbers.__contains__(0): numbers.remove(0)
 
 print(numbers)
