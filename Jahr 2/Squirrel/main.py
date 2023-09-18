88de1f93dcf34407effee961adf0a4fd1e43df00
Goblinmine <jojo.millonig@gmail.com>
2023-09-18 16:33:13 +0200

    Finished squirrel

--- Jahr 2/Squirrel/main.py
+++ Jahr 2/Squirrel/main.py
@@ -3,5 +3,27 @@
 # Creation date:    18. 09. 2023
 
 import pandas
-# drei hauptfarben. zählen wie viele es in welcher farbe giebt. danach auslesen
+import numpy as np
 
+output = {}
+data = pandas.read_csv('Jahr 2/Squirrel/data.csv')
+
+# counting colors
+for sqr_color in data['Primary Fur Color']:
+    if sqr_color is np.nan: continue
+    if sqr_color not in output.keys():
+        output[sqr_color] = 1
+    else:
+        output[sqr_color] += 1
+
+# format into nice csv
+export = {
+    'color':[],
+    'amount':[]
+}
+
+for color in output:
+    export['color'].append(color)
+    export['amount'].append(output[color])
+
+pandas.DataFrame(export).to_csv('Jahr 2/Squirrel/output.csv')
