9ae3e3282e8da6c7a360792552f2e332db4fed8b
Goblinmine <jojo.millonig@gmail.com>
2023-09-18 15:34:10 +0200

    added Squirrel exercise

---
+++ Jahr 2/Squirrel/main.py
@@ -0,0 +1,7 @@
+# Exercise:         squirrel
+# Created by:       Jonas Millonig
+# Creation date:    18. 09. 2023
+
+import pandas
+# drei hauptfarben. zählen wie viele es in welcher farbe giebt. danach auslesen
+
