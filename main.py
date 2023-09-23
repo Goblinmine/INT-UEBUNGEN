3dde1874796640a73959043f58055894146dfab4
Jonas <jonas.millonig@lisa-software.eu>
2023-09-23 07:20:52 +0200

    added .vscode to gitignore; formated main & main2

--- main.py
+++ main.py
@@ -2,27 +2,28 @@ import pandas
 import matplotlib.pyplot as plt
 
 data = pandas.read_csv("data/Seen_Morphometrie.csv", encoding='ANSI', sep=";")
-#print(data)
+# print(data)
 
 myFiles = []
 years = []
 
 for year in range(2007, 2023):
-    myFiles.append(pandas.read_csv(f"data/Seenberichtsdaten Jahr {year}.csv", encoding='ANSI', sep=";"))
+    myFiles.append(pandas.read_csv(
+        f"data/Seenberichtsdaten Jahr {year}.csv", encoding='ANSI', sep=";"))
     years.append(year)
 
 for dataFrames in myFiles:
-    mask = dataFrames['MESSSTELLE'].str.contains('Magdalenensee|Vassacher See|Silbersee')
-    rowsToDelete = dataFrames[~mask]    
+    mask = dataFrames['MESSSTELLE'].str.contains(
+        'Magdalenensee|Vassacher See|Silbersee')
+    rowsToDelete = dataFrames[~mask]
     deletedRows = dataFrames.drop(rowsToDelete.index)
-    #print(deletedRows)
-    
+    # print(deletedRows)
 
 
-see1={
+see1 = {
     "Wassertemperatur": [],
-    "Sichttiefe" : [], 
-    "ph-Wert" : [],
+    "Sichttiefe": [],
+    "ph-Wert": [],
     "Elektr. Leitfaehigkeit": [],
     "Phosphor": [],
     "Nitrat-Stickstoff": [],
@@ -33,19 +34,17 @@ see1={
     "Chlorophyll": []}
 
 
-
-
-
-#print(years)
+# print(years)
 year07 = myFiles[0]
-mask =(year07['MESSSTELLE'].str.contains('Magdalenensee'))  & (year07["PARAMETERBEZEICHNUNG"] == "Wassertemperatur")
+mask = (year07['MESSSTELLE'].str.contains('Magdalenensee')) & (
+    year07["PARAMETERBEZEICHNUNG"] == "Wassertemperatur")
 see1["Wassertemperatur"] = mask
 print(see1)
-#print(year07[mask])
+# print(year07[mask])
 
-#rowsToDelete = year07[~mask]
+# rowsToDelete = year07[~mask]
 
-#test = year07.drop(rowsToDelete.index)
+# test = year07.drop(rowsToDelete.index)
 
-#print(test)
+# print(test)
 # DELETE FROM data WHERE Messstelle != nameSee
