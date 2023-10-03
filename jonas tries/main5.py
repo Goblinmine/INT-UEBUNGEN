6878694b92b5463141e83e1cb3974f528ebdadcc
Jonas <jonas.millonig@lisa-software.eu>
2023-10-03 23:43:56 +0200

    tryed something with main5.py

--- jonas tries/main5.py
+++ jonas tries/main5.py
@@ -56,12 +56,55 @@ def get_phytoplankton():
 
 def main():  
     read_files()
-    output = get_lake_biggest_dif()     
+    output = get_lake_biggest_dif()
+    ax: Axes
+    fig, ax = plt.subplots(layout='constrained')
     plt.suptitle('Title', fontsize=16)
     print(output)
-    plt.plot(output.RELATIVE_ABFLUSS, output.AVG_Phytoplankton, scalex=output.index.tolist(), marker='o', linestyle='')
-    plt.xlabel('Relativer Abfluss (l/s/m³)')
-    plt.ylabel('AVG Phytoplankton Biovolumen (mm³/l)')
+    
+    # output[['RELATIVE_ABFLUSS', 'AVG_Phytoplankton']].to_csv("output.csv")
+    
+    
+    
+    ax.plot(output.RELATIVE_ABFLUSS, output.AVG_Phytoplankton, marker='o', linestyle='')
+    ax.set_xticks(output.RELATIVE_ABFLUSS)
+    ax.set_xticklabels(output.index.tolist(), rotation=90)
+    ax.set_xlabel('Relativer Abfluss (l/s/m³ * 10000)')
+    ax.set_ylabel('AVG Phytoplankton Biovolumen (mm³/l)')
     plt.show()
     
+    
 main()
+    
+    
+# def test():
+#     read_files()
+#     output = get_lake_biggest_dif()
+#     print(output)
+#     ax: Axes
+#     fig, ax = plt.subplots(layout='constrained')
+#     plt.suptitle('Title', fontsize=16)
+    
+#     x = np.arange(len(output))  # the label locations
+#     width = 0.25  # the width of the bars
+#     multiplier = 0
+
+
+#     for row in output[['RELATIVE_ABFLUSS', 'AVG_Phytoplankton']].itertuples():
+#         offset = width * multiplier
+#         rects = ax.bar(x + offset, row[1], width, label=row[1])
+#         # ax.bar_label(rects, padding=3)
+#         multiplier += 1
+        
+#     # Add some text for labels, title and custom x-axis tick labels, etc.
+#     ax.set_ylabel('Length (mm)')
+#     ax.set_title('Penguin attributes by species')
+#     ax.set_xticks(x + width, output.index.tolist())
+#     ax.legend(loc='upper left', ncols=3)
+#     # ax.set_ylim(0, 250)
+
+#     plt.show()
+    
+
+    
+# test()
