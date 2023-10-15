2028c6b2bddf6be5f21d28e513f29315ba490b45
Jonas <jonas.millonig@lisa-software.eu>
2023-10-16 00:19:41 +0200

    Added docstr

--- Jahr 2/api_iss/main.py
+++ Jahr 2/api_iss/main.py
@@ -1,9 +1,32 @@
 import requests
 from datetime import datetime as dt, timedelta
+import math
 
 DEBUG = False
 
 def is_night(pos: tuple[float, float]) -> bool:
+    """
+    Determine if it is nighttime at a specific geographic location.
+
+    This function checks if it is currently nighttime at the given longitude
+    and latitude coordinates. It does so by querying the Sunrise-Sunset API
+    for the sunset and sunrise times for the location and comparing them to the
+    current time in UTC.
+
+    Args:
+        pos (tuple[float, float]): A tuple containing longitude and latitude.
+
+    Returns:
+        bool: True if it is nighttime; otherwise, False.
+
+    Raises:
+        requests.exceptions.RequestException: If an error occurs while making the HTTP request.
+
+    Example:
+    >>> position = (37.7749, -122.4194)  # San Francisco, CA coordinates
+    >>> is_night(position)
+    False  # It is not nighttime in San Francisco.
+    """
     
     # We don't need to worry about time zones and daylight saving time
     # because the Sunrise API provides its responses in UTC time without any offsets
@@ -29,11 +52,49 @@ def is_night(pos: tuple[float, float]) -> bool:
 
 # split in iss_overhead and get_iss_position for easier use in part II
 
-def iss_overhead(pos: tuple[float, float]) -> bool:
+def iss_overhead(pos: tuple[float, float], range = 10) -> bool:
+    """
+    Determine if the International Space Station (ISS) is overhead at a given location.
+
+    This function checks whether the ISS is passing overhead a specific location on Earth
+    based on its longitude and latitude. It considers the ISS as overhead
+    if it is within the specified range from the given coordinates.
+
+    Args:
+        pos (tuple[float, float]): A tuple containing your longitude and latitude.
+        visible_range (float): The range in which the ISS is considered overhead (default is 10 units).
+
+    Returns:
+        bool: True if the ISS is overhead; otherwise, False.
+
+    Example:
+    >>> iss_overhead((45.0, -75.0), 20.0)
+    True  # ISS is overhead within a range of 20 units.
+    """
     iss_pos = get_iss_position()
-    return (((iss_pos[1] <= pos[1]+5) and (iss_pos[1] >= pos[1]-5)) and ((iss_pos[0] <= pos[0]+5) and (iss_pos[0] >= pos[0]-5)))
+    distance = math.sqrt((pos[0] - iss_pos[0]) ** 2 + (pos[1] - iss_pos[1]) ** 2)
+    
+    return distance < range
 
 def get_iss_position() -> tuple[float, float]:
+    """
+    Retrieve the current International Space Station (ISS) position.
+
+    This function sends a request to the Open Notify API to obtain the current location
+    of the ISS in terms of latitude and longitude. It then parses the response and
+    returns a tuple containing the ISS's latitude and longitude.
+
+    Returns:
+        tuple[float, float]: A tuple containing the ISS's longitude and latitude.
+
+    Raises:
+        requests.exceptions.RequestException: If an error occurs while making the HTTP request.
+
+    Example:
+    >>> position = get_iss_position()
+    >>> print(f'Current ISS Position: Latitude {position[1]}, Longitude {position[0]}')
+    Current ISS Position: Latitude 51.6499, Longitude 0.0473
+    """
     response_iss = requests.get('http://api.open-notify.org/iss-now.json')
     response_iss.raise_for_status()
 
--- Jahr 2/api_iss/main2.py
+++ Jahr 2/api_iss/main2.py
@@ -4,7 +4,80 @@ import math
 
 THREASHOLD = 10
 
-def setup_turtles():
+# penup if moving distance is higher then THREASHOLD
+def goto_threashold(pos: tuple[float, float], turtle: turtle.Turtle) -> None:
+    """
+    Move a Turtle graphics object to a new position if it is beyond a threshold distance.
+
+    This function calculates the Euclidean distance between the current position of the Turtle
+    and the specified position. If the distance is greater than a defined threshold (THRESHOLD),
+    the Turtle is lifted (penup), and if not, it is lowered (pendown) and moved to the new position.
+
+    Args:
+        pos (tuple[float, float]): A tuple containing the target position (x, y).
+        turtle (turtle.Turtle): The Turtle graphics object to be moved.
+
+    Returns:
+        None
+
+    Example:
+    >>> my_turtle = turtle.Turtle()
+    >>> target_position = (50, 50)
+    >>> goto_threshold(target_position, my_turtle)
+    # The Turtle will be lifted (penup) if the distance to the target is greater than THRESHOLD,
+    # or lowered (pendown) and moved to the target position otherwise.
+    """
+    current_pos = turtle.pos()
+    if math.sqrt((pos[0] - current_pos[0])**2 + (pos[1] - current_pos[1])**2) > THREASHOLD: turtle.penup()
+    else: turtle.pendown()
+    turtle.goto(pos)
+    
+    
+def __write_night(pos: tuple[float, float], text: turtle.Turtle) -> None:
+    """
+    Display 'Night' or 'Day' on a Turtle graphics screen based on the time at a specific location.
+
+    This function determines whether it is currently nighttime at the specified longitude
+    and latitude coordinates using the 'api.is_night' function. It then uses a Turtle graphics
+    object to display 'Night' or 'Day' on the screen accordingly.
+
+    Args:
+        pos (tuple[float, float]): A tuple containing longitude and latitude.
+        turtle (turtle.Turtle): The Turtle graphics object used for text display.
+
+    Returns:
+        None
+
+    Example:
+    >>> my_turtle = turtle.Turtle()
+    >>> target_position = (37.7749, -122.4194)  # San Francisco, CA coordinates
+    >>> __write_night(target_position, my_turtle)
+    # This function will update the Turtle graphics screen to display 'Night' or 'Day'.
+    """
+    pos_offset = (pos[0] - 2, pos[1] - 15)
+    text.goto(pos_offset)
+    is_night = api.is_night(pos)
+    text.clear()
+    text.write('Night' if is_night else 'Day')
+    
+    
+def iss_loop():
+    """
+    Continuously update the position of the International Space Station (ISS) on a Turtle graphics screen.
+
+    This function sets up a Turtle graphics screen and displays the current position of the ISS
+    by continuously updating the Turtle's position. It also displays 'Night' or 'Day' based on the
+    time at the ISS's location.
+
+    Args: None
+
+    Returns:
+        None
+
+    Example:
+    >>> iss_loop()
+    # This function will continuously update the ISS's position and time information on the screen.
+    """
     screen = turtle.Screen()
     screen.setup(1000,500)
     screen.bgpic('Jahr 2/api_iss/earth.gif')
@@ -18,31 +91,15 @@ def setup_turtles():
     text.hideturtle()
     text.penup()
     
-    return screen, iss, text
-
-# penup if moving distance is higher then THREASHOLD
-def goto_threashold(pos: tuple[float, float], turtle: turtle.Turtle) -> None:
-    current_pos = turtle.pos()
-    if math.sqrt((pos[0] - current_pos[0])**2 + (pos[1] - current_pos[1])**2) > THREASHOLD: turtle.penup()
-    else: turtle.pendown()
-    turtle.goto(pos)
-    
-    
-def write_night(pos: tuple[float, float], turtle: turtle.Turtle) -> None:
-    pos_offset = (pos[0] - 2, pos[1] - 15)
-    turtle.goto(pos_offset)
-    is_night = api.is_night(pos)
-    turtle.clear()
-    turtle.write('Night' if is_night else 'Day')
-
-
-def main():
-    screen, iss, text = setup_turtles()
-    pos: tuple[float, float]
     while True:
         pos = api.get_iss_position()
         goto_threashold(pos, iss)
-        write_night(pos, text)
+        __write_night(pos, text)
+
+
+def main():
+    iss_loop()
+        
 
 if __name__ == '__main__':
     main()
