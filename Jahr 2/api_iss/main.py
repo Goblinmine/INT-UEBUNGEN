ddba6d9ab740aa0a4faf1c8beb5bab54a3974358
Jonas <jonas.millonig@lisa-software.eu>
2023-10-15 22:35:37 +0200

    fixed timezone problem

--- Jahr 2/api_iss/main.py
+++ Jahr 2/api_iss/main.py
@@ -1,6 +1,5 @@
 import requests
 from datetime import datetime as dt, timedelta
-import pytz
 
 DEBUG = False
 
@@ -10,31 +9,20 @@ def is_night(lat: float, lng: float) -> bool:
         'lng':lng,
         'formatted': 0
     }
-    now = get_time(lat, lng)
+    now_utc = dt.utcnow()
     datetime_format = "%Y-%m-%dT%H:%M:%S%z"
 
     response = requests.get('https://api.sunrise-sunset.org/json', parameters)
     response.raise_for_status()
 
-    sunset = dt.strptime(response.json()['results']['sunset'], datetime_format)
-    sunrise = dt.strptime(response.json()['results']['sunrise'], datetime_format)
-    sunrise_ofset = sunrise + timedelta(days=1)
+    sunset = dt.strptime(response.json()['results']['sunset'], datetime_format).replace(tzinfo=None)
+    sunrise = dt.strptime(response.json()['results']['sunrise'], datetime_format).replace(tzinfo=None)
+    sunrise_offset = sunrise + timedelta(days=1)
 
-    return (now > sunset) and (now < sunrise_ofset)
-
-
-def get_time(lat: float, lng: float) -> dt:
-    parameters = {
-        'key':'R7TIXHW7ALW6',
-        'format':'json',
-        'by':'position',
-        'lat':lat,
-        'lng':lng,
-    }
-    result = requests.get('http://api.timezonedb.com/v2.1/get-time-zone', parameters)
-    result.raise_for_status()
-    
-    return dt.now(tz=pytz.timezone(result.json()['zoneName']))
+    # night = (now > sunset) and (now < sunrise_ofset)
+    night = sunset < now_utc < sunrise_offset
+    if DEBUG: print(f'DEBUG: night -> sunset: {sunset}, now_utc: {now_utc} , sunrise_offset: {sunrise_offset}, output: {night}')
+    return night
 
 
 def iss_overhead(lat: float, lng: float) -> bool:
@@ -48,6 +36,8 @@ def get_iss_position() -> tuple[float, float]:
     iss_lat = float(response_iss.json()['iss_position']['latitude'])
     iss_lng = float(response_iss.json()['iss_position']['longitude'])
     
+    if DEBUG: print(f'DEBUG: ISS pos -> lat: {iss_lat}, lng: {iss_lng}')
+    
     return iss_lat, iss_lng
 
     
@@ -56,11 +46,13 @@ def main(lat: float, lng: float, debug_output = False) -> None:
     DEBUG = debug_output
 
     try:
-        if not is_night(lat, lng): exit()
+        if not is_night(lat, lng):
+            print('You are not gonna see the ISS because its day!')
+            return
         print(f'ISS is{"" if iss_overhead(lat, lng) else " not"} over your head!!')
             
     except Exception as ex:
         print(f'ERROR: {ex}')
 
 if __name__ == '__main__':
-    main(lat=46.636459, lng=14.312225, debug_output=True)
+    main(lat=46.636459, lng=14.312225)
